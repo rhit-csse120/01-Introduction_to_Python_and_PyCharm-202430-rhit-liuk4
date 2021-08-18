a0ab59f61434a541702b95aac7085ca786fea384
David Mutchler <mutchler@rose-hulman.edu>
2021-08-18 14:32:14 -0400

    NOT ready for review.

---
+++ src/m1e_comments_strings_print.py
@@ -0,0 +1,37 @@
+###############################################################################
+#
+#   Read the code below.  Predict (in your head)
+#   what will get printed (i.e., displayed) when the code runs.
+#
+#   Then run this module by right clicking anywhere in this window
+#   and then selecting:
+#        Run 'name of file'
+#   (i.e.  Run 'm1e_comments_strings...')
+#
+#       Or, use the Windows keyboard shortcut:  Control + Shift + Function-F10
+#       That is, hold the Control, Shift and Function keys down
+#       and press the F10 key.
+#
+#       To run the SAME module again, you can press the "Play" button on
+#       the toolbar at the top of this window (that is, the green,
+#       rightward-pointing triangle).
+#
+#   Confirm that you see the output in the  "Run"  window that appears.
+#       Note: We recommend that you click on the "Gear" symbol at the top-right
+#       part of that window and select
+#           Move to  ~  Right Top
+#       to make the output easier to see.
+#   Confirm that the output is what you expected, asking questions as desired.
+#
+#   This module is just an example (m1e, note the   e   ).
+#   After you have read and run the code,
+#   feel free to play with it briefly, then move on to m2.
+#
+###############################################################################
+
+print("Hello, World")
+print("hi there")
+print("one", "two", "buckle my shoe")
+
+print(3 + 9)
+print("3 + 9", "versus", 3 + 9)
