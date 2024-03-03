5d142d8d8649ce0b251a46804942d7a468e8585b
liuk4 <liuk4@rose-hulman.edu>
2024-03-02 19:16:07 -0500

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -15,7 +15,7 @@
 print("Hello, World")
 
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #  _
 #   Run this module by right clicking anywhere in this window
@@ -29,7 +29,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing "to do" in this module.
 #  _
@@ -52,21 +52,21 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
-#  _
+print("Like tears, in rain...")
 #   Test your code by re-running this module.
 #   Look at the  Run  window to be sure that your string printed as expected.
 ###############################################################################
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT need strings for this, so no quotation marks!
-#  _
+print("Time " + str(3067 * 34227))
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:
 #      What number did your   print   display for this _TODO_?
@@ -74,7 +74,7 @@ print("Hello, World")
 ###############################################################################
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
